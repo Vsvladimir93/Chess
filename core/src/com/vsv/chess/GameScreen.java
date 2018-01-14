97dee4652acea16e354aea592712c2581cf57074
V.Vladimir <vsvladimir93@gmail.com>
2018-01-15 00:40:44 +0300

    1

--- core/src/com/vsv/chess/GameScreen.java
+++ core/src/com/vsv/chess/GameScreen.java
@@ -3,7 +3,9 @@ package com.vsv.chess;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,21 +19,98 @@ public class GameScreen implements Screen {
     OrthographicCamera camera;
     Texture texture;
     Map<String, Tile> tileList;
-    Integer counterX = 0;
-    Integer counterY = 0;
-    long time;
-    boolean bool = false;
+    TextureAtlas textureAtlas;
+    Sprite rookImg;
 
     public GameScreen() {
         camera = new OrthographicCamera();
         batch = new SpriteBatch();
-
+        textureAtlas = new TextureAtlas(Gdx.files.internal("Chess.txt"));
+        rookImg = textureAtlas.createSprite("rook_white");
         tileList = new HashMap();
         createTiles();
 
     }
 
-    void createTiles() {
+    public int getPositionX() {
+        Double x = 0.0;
+
+        if (Gdx.input.justTouched()) x = Gdx.input.getX() + 0.0;
+
+        x = x / 64;
+
+        x = Math.floor(x);
+
+        return x.intValue();
+    }
+
+    public int getPositionY() {
+
+        Double y = 0.0;
+
+        if (Gdx.input.justTouched()) y = Gdx.input.getY() + 0.0;
+
+        //инвертирую число Y потому что координаты нулевые координаты Y мыши начинаются с верхнего левого угла.
+        y = y / 64;
+        y = Math.floor(y);
+        y = y - 7;
+        y = Math.abs(y);
+
+        return y.intValue();
+    }
+
+    @Override
+    public void show() {}
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        batch.begin();
+
+        drawTiles();
+        batch.draw(rookImg, 0, 0);
+
+
+
+
+/*
+            System.out.println(getPositionX());
+            System.out.println(getPositionY());
+*/
+        batch.end();
+
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+
+    }
+
+    public void createTiles() {
         for (int vert = 0; vert < 8; vert++) {
             for (int hor = 0; hor < 8; hor++) {
                 if (vert % 2 == 0 && hor % 2 == 0) {
@@ -68,110 +147,5 @@ public class GameScreen implements Screen {
 
             }
         }
-    }
-
-    public int getPositionX() {
-        Double x = 0.0;
-
-        if (Gdx.input.justTouched()) x = Gdx.input.getX() + 0.0;
-
-        x = x / 64;
-
-        x = Math.floor(x);
-
-        return x.intValue();
-    }
-
-    public int getPositionY() {
-
-        Double y = 0.0;
-
-        if (Gdx.input.justTouched()) y = Gdx.input.getY() + 0.0;
-
-        //инвертирую число Y потому что координаты нулевые координаты Y мыши начинаются с верхнего левого угла.
-        y = y / 64;
-        y = Math.floor(y);
-        y = y - 7;
-        y = Math.abs(y);
-
-        return y.intValue();
-    }
-
-
-    @Override
-    public void show() {
-        time = System.currentTimeMillis();
-    }
-
-    public void painter(boolean bool) {
-        if (bool) {
-            if (System.currentTimeMillis() - time > 700) {
-                String s = counterX.toString() + counterY.toString();
-                tileList.get(s).changeColor(Color.BLUE);
-                counterX += 1;
-                time = System.currentTimeMillis();
-                if (counterX == 8) {
-                    counterX = 0;
-                    counterY += 1;
-                }
-                if (counterY == 8) {
-                    Gdx.app.exit();
-                }
-
-            }
-        }
-    }
-
-    @Override
-    public void render(float delta) {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        camera.update();
-        batch.begin();
-
-        drawTiles();
-
-if(Gdx.input.justTouched()){bool = true;}
-        painter(bool);
-
-
-
-
-
-/*
-            System.out.println(getPositionX());
-            System.out.println(getPositionY());
-*/
-
-
-        batch.end();
-
-
-    }
-
-    @Override
-    public void resize(int width, int height) {
-
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
-
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-
     }
 }
